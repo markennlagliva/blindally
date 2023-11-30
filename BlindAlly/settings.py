334453a62289493bd2877d7c573eded65d9cfaf1
Markenn <markennlagliva@gmail.com>
2023-11-30 16:43:04 +0800

    3-try

--- BlindAlly/settings.py
+++ BlindAlly/settings.py
@@ -117,13 +117,13 @@ class Dev(Configuration):
     # Internationalization
     # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-    LANGUAGE_CODE = values.Value('en-us')
+    LANGUAGE_CODE = 'en-us'
 
     TIME_ZONE = values.Value('UTC')
 
-    USE_I18N = values.BooleanValue(True)
+    USE_I18N = True
 
-    USE_TZ = values.BooleanValue(True)
+    USE_TZ = True
 
 
     # Static files (CSS, JavaScript, Images)
