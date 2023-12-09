672117f0e608cba0fc986c872dc88d850d908032
Markenn <markennlagliva@gmail.com>
2023-12-09 14:24:15 +0800

    --all functions implemented

--- AssistApp/urls.py
+++ AssistApp/urls.py
@@ -3,7 +3,24 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    # Info Apps
     path('newsfeed', views.newsfeed,  name='newsfeed'),
     path('current-time-date', views.current_time_date,  name='current_time_date'),
     path('location', views.location,  name='location'),
+    path('assistant-bot', views.assistantbot,  name='assistantbot'),
+
+    # Audio Apps
+    path('audio-book', views.audio_book, name='audio_book'),
+    path('audio-music', views.audio_music, name='audio_music'),
+
+    # More
+    path('motivation', views.motivation,  name='motivation'),
+    path('technologies', views.technologies,  name='technologies'),
+
+    # About 
+    path('user-guidelines', views.guidelines,  name='guidelines'),
+
+
+
 ]
