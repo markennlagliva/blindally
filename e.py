6f1cf51fb229f2a3f72c5fa5b2c84268b0504992
Markenn <markennlagliva@gmail.com>
2024-01-04 09:56:58 +0800

    tl --filipino accent

--- e.py
+++ e.py
@@ -19,16 +19,40 @@
 # elevenlabs.play(audio)
 
 # from playsound import playsound
+import gtts
+from gtts import gTTS
+from playsound import playsound
+text = 'Kamusta kayo lahat? Ako nga pala si blind ally ang inyong tagapaglingkod.'
+audio = gTTS(text, lang='tl') # 1. com.mx, 2. pt
+audio.save('audio/ex.mp3')
+playsound('audio/ex.mp3')
 
-# from gtts import gTTS
-# text = 'This is blind ally'
-# audio = gTTS(text)
-# audio.save('audio/ex.mp3')
-# playsound('audio/ex.mp3')
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
-load_dotenv()
-print(os.getenv('SECRET_KEY'))
+# load_dotenv()
+# print(os.getenv('SECRET_KEY'))
+
+# print(help(gtts.lang))
+
+from gtts.lang import tts_langs
+# print(tts_langs())
+
+for k, v in tts_langs().items():
+    print(k, v)
+
+'''
+    >>> from gtts import gTTS
+>>> from io import BytesIO
+>>>
+>>> mp3_fp = BytesIO()
+>>> tts = gTTS('hello', lang='en')
+>>> tts.write_to_fp(mp3_fp)
+>>>
+>>> # Load `mp3_fp` as an mp3 file in
+>>> # the audio library of your choice
+'''
+
+
 
 
