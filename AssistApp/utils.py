0491a570450df985b8a8b81309e5a65b6ff06f0c
markennlagliva <markennlagliva@gmail.com>
2024-01-30 22:51:32 +0800

    --server side validation for language mode

--- AssistApp/utils.py
+++ AssistApp/utils.py
@@ -8,9 +8,8 @@ import os
 # lang = 'tl' or English
 
 class LanguageMode:
-    def __init__(self, details, status) -> None:
+    def __init__(self, details) -> None:
         self.details = details
-        self.status = status
 
     def english_speak(self):
         import pyttsx3
@@ -25,7 +24,7 @@ class LanguageMode:
     def tagalog_speak(self):
         from googletrans import Translator
         result = Translator().translate(self.details[0] + self.details[1], dest='tl')
-
+        print('This is inside tagalog.', result.text)
         # This is for Filipino Language
         audio = gTTS(result.text, lang='tl', slow=False)
         audio.save('audio/playsound.mp3')
@@ -34,28 +33,32 @@ class LanguageMode:
 
 
 # Status: True = English || False = Tagalog
-def speak(response, details=None, status=False):
+def speak(response, details=None, status=True):
     # Home Page || Base
-    
-    if response == 'home':
-        audio = gTTS('You are currently in Home page', lang='en')
-        
-        
-    elif response == 'tap-detail-home':
-        pass
-        
+    print('This is the status server:', status)
+    if status == False:
+        if response == 'tap-detail-home':
+            LanguageMode(details).tagalog_speak()
 
-    # More Page
-    elif response == 'motivation':
-        audio = gTTS('You are currently in Motivation Page', lang='en-au', slow=False, tld='us')
-    
-    elif response == 'tap-more-motivation':
-        audio = gTTS(details[0] + '. So' +details[1], lang='en-au', slow=False, tld='us')
+        
+    else:
+        if response == 'home':
+            LanguageMode(details).english_speak() 
+                    
+        elif response == 'tap-detail-home':
+            LanguageMode(details).english_speak()
+            
 
-    elif response == 'technologies':
-        audio = gTTS('You are currently in Motivation Page', lang='en-au', slow=False, tld='us') 
-    
-    elif response == 'tap-more-technologies':
-        audio = gTTS(details[0], lang='en-au', slow=False, tld='us')
-   
+        # More Page
+        elif response == 'motivation':
+            LanguageMode(details).english_speak()
+        
+        elif response == 'tap-more-motivation':
+            LanguageMode(details).english_speak()
 
+        elif response == 'technologies':
+            LanguageMode(details).english_speak()
+        
+        elif response == 'tap-more-technologies':
+            LanguageMode(details).english_speak()
+        
