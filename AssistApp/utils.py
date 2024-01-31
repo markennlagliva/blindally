285eed79a3f94dc5b1454b854ba4233ca3a9833c
markennlagliva <markennlagliva@gmail.com>
2024-01-31 20:24:55 +0800

    --voice navigation w/ language mode

--- AssistApp/utils.py
+++ AssistApp/utils.py
@@ -15,22 +15,21 @@ class LanguageMode:
         import pyttsx3
         engine = pyttsx3.init()
         voices = engine.getProperty('voices')
-        engine.setProperty('rate', 170)
+        engine.setProperty('rate', 165)
         engine.setProperty('voice', voices[1].id)
 
-        print('This is the length of details.', len([(self.details)]), type(self.details))
-        if len([self.details]) >= 2:
-            engine.say(self.details[0] + '. and' +self.details[1])
-        engine.say(self.details)
+        # print('This is the length of details.', len([(self.details)]), type(self.details))
+        engine.say(' '.join(self.details))
+   
         engine.runAndWait()
         engine.stop()
     
     def tagalog_speak(self):
         from googletrans import Translator
-        if len([self.details]) >= 2:
-            result = Translator().translate(self.details[0] + self.details[1], dest='tl')
-        result = Translator().translate(str(self.details), dest='tl')
-        print('This is inside tagalog.', result.text)
+     
+        result = Translator().translate(' '.join(self.details), dest='tl')
+
+        # print('This is inside tagalog.', result.text)
         # This is for Filipino Language
         audio = gTTS(result.text, lang='tl', slow=False)
         audio.save('audio/playsound.mp3')
@@ -44,8 +43,7 @@ def speak(response, details=None, status=True):
     print('This is the status server:', status)
     if status:
         if response == 'home':
-            details = 'Proceeding to home page.'
-            LanguageMode(details).english_speak() 
+            LanguageMode(['Proceeding to Home page.']).english_speak() 
                     
         elif response == 'tap-detail-home':
             LanguageMode(details).english_speak()
@@ -53,20 +51,33 @@ def speak(response, details=None, status=True):
 
         # More Page
         elif response == 'motivation':
-            LanguageMode(details).english_speak()
+            LanguageMode(['Going to motivation page.']).english_speak()
         
         elif response == 'tap-more-motivation':
             LanguageMode(details).english_speak()
 
         elif response == 'technologies':
-            LanguageMode(details).english_speak()
+            LanguageMode(['Going to tehcnologies page.']).english_speak()
         
         elif response == 'tap-more-technologies':
             LanguageMode(details).english_speak()  
     else:
         if response == 'home':
-            details = 'Nasa Home Page na tayo.'
-            LanguageMode(details).tagalog_speak()
+            LanguageMode(['Nasa Home page ka.']).tagalog_speak()
+
         elif response == 'tap-detail-home':
             LanguageMode(details).tagalog_speak()
+
+        
+        elif response == 'motivation':
+            LanguageMode(['Nasa motivation page ka.']).tagalog_speak()
+        
+        elif response == 'tap-more-motivation':
+            LanguageMode(details).tagalog_speak()
+
+        elif response == 'technologies':
+            LanguageMode(['Nasa technologies page ka.']).tagalog_speak()
+        
+        elif response == 'tap-more-technologies':
+            LanguageMode(details).tagalog_speak()  
         
