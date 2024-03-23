1723ef693312bd6d878ed977080d9e92c77a18b2
markennlagliva <markennalagliva@gmail.com>
2024-03-23 12:59:26 +0800

    debug deployment SS

--- BlindAlly/settings.py
+++ BlindAlly/settings.py
@@ -140,16 +140,17 @@ class Dev(Configuration):
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-    STATIC_URL = 'static/'
+    # STATIC_URL = 'static/'
 
 
     STATIC_URL = '/static/'
-    # MEDIA_URL = 'media/'
 
     STATIC_ROOT = BASE_DIR / "staticfiles"
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static")
     ]
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 
     # Default primary key field type
